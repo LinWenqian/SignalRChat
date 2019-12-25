4e6df0929a1d03e6e2028c130248e72568dd3436
LinWenqian <warmqian@163.com>
2019-12-25 14:10:10 +0800

    no message

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
